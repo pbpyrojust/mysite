3a96e35f5aa373330bf30167ff24e95f9256587f
Justin Adams <jadams@bepress.com>
2014-02-21 13:13:42 -0800

    Boobs

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from polls import views
 
 urlpatterns = patterns('',
-	# ex: /polls/
+    # ex: /polls/
     url(r'^$', views.index, name='index'),
     # ex: /polls/5/
     url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
