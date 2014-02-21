977fc6d155ba1527ad2f77d84c27a153f8be1725
Justin Adams <jadams@bepress.com>
2014-02-20 17:10:26 -0800

    Creating the 404 view and template

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
@@ -10,11 +11,16 @@ def index(request):
 	context = RequestContext(request, {
 		'latest_poll_list': latest_poll_list,
 	})
-	output = ', '.join([p.question for p in latest_poll_list])
+	#output = ', '.join([p.question for p in latest_poll_list])
 	return HttpResponse(template.render(context))
 
 def detail(request, poll_id):
-	return HttpResponse("You're looking at poll %s." % poll_id)
+	try:
+		poll = Poll.objectsget(pk=poll_id)
+	except Poll.DoesNotExist:
+		raise Http404
+	#return HttpResponse("You're looking at poll %s." % poll_id)
+	return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
 	return HttpResponse("You're looking at results of pll %s." % poll_id)
