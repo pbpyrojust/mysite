1668e8f6ce0925533de731d2665ad7fae9b2f3e4
Justin Adams <jadams@bepress.com>
2014-02-25 10:50:18 -0800

    Amending views

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reversedfrom django.views import generic
+from django.core.urlresolvers import reverse
+from django.views import generic
 
 from polls.models import Choice, Poll
 
 class IndexView(generic.ListView):
-	template
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
 
+    def get_queryset(self):
+        """Return the last five published polls."""
+        return Poll.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
 
 def vote(request, poll_id):
 	p = get_object_or_404(Poll, pk=poll_id)
